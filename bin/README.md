2ab349025b2bc6267df9f2ceb9fb1eff2ece9be7
Omar Juma <jumao2669@gmail.com>
2024-09-20 19:43:08 +0300

    first commit

---
+++ bin/README.md
@@ -0,0 +1,4 @@
+# `photoz-clone`
+
+This is a tiny Google Photos-like clone built with the **Spring Boot Tutorial - Crash Course** made by **Marco Codes**,
+available on **[YouTube](https://www.youtube.com/watch?v=QuvS_VLbGko)**.
---
+++ src/main/java/photozclone/PhotozController.java
@@ -0,0 +1,53 @@
+package photozclone;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class PhotozController {
+    private final PhotozService photozService;
+
+    public PhotozController(PhotozService photozService) {
+        this.photozService = photozService;
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello There, This is a simple application. To begin, type /upload.html on your browser url section to upload a photo";
+    }
+
+    @GetMapping("/photoz")
+    public Collection<Photo> get() {
+        return photozService.get();
+    }
+
+    @GetMapping("/photoz/{id}")
+    public Photo get(@PathVariable String id) {
+        Photo photo = photozService.get(id);
+        if (photo == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
+    }
+
+    @DeleteMapping("/photoz/{id}")
+    public void delete(@PathVariable String id) {
+        Photo photo = photozService.remove(id);
+        if (photo == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/photoz")
+    public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+        return photozService.save(file.getOriginalFilename(), file.getBytes());
+    }
+}
